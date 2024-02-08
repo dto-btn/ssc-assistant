e1dca190afa96bba7847e60616ce294933889892
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-02-08 10:21:11 -0500

    added keyvault

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,43 @@ resource "azurerm_storage_account" "main" {
   account_replication_type = "LRS"
 }
 
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                        = "${replace(var.project_name, "_", "")}keyvault"
+  location                    = azurerm_resource_group.main.location
+  resource_group_name         = azurerm_resource_group.main.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+
+  #TODO: add desired policies ...
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get", "Set", "List"
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "jwtsecret" {
+  name         = "jwt-secret"
+  value        = var.jwt_secret
+  key_vault_id = azurerm_key_vault.main.id
+}
+
 /****************************************************
 *                        DNS                        *
 *****************************************************/
