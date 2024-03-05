ac50b4d279dc6ada9fce88dcd5efd75072366b6f
danielsamchenko <samchenkodaniel@gmail.com>
2024-03-05 00:22:59 -0500

    Dial moves to top right on mobile/smaller view

--- app/frontend/src/components/Dial.tsx
+++ app/frontend/src/components/Dial.tsx
@@ -1,13 +1,15 @@
 import React from 'react';  
-import { SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material';  
+import { SpeedDial, SpeedDialAction, SpeedDialIcon, useMediaQuery, useTheme } from '@mui/material';  
 import DeleteIcon from '@mui/icons-material/Delete';  
   
-export interface DialProps {  
-  onClearChat: () => void; // This function will be passed as a prop to handle the "Clear Chat" action.  
+interface DialProps {  
+  onClearChat: () => void;
 }  
   
-export const Dial: React.FC<DialProps> = ({ onClearChat }) => {  
+export const Dial = ({ onClearChat}: DialProps) => {  
   const [open, setOpen] = React.useState(false);  
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm')); // check if screen is small
   
   const handleClose = () => {  
     setOpen(false);  
@@ -21,25 +23,26 @@ export const Dial: React.FC<DialProps> = ({ onClearChat }) => {
     { icon: <DeleteIcon />, name: 'Clear Chat', handler: onClearChat },  
   ];  
   
-  return (  
-    <div style={{ position: 'fixed', bottom: '16px', right: '16px', zIndex: 2000 }}>  
-      <SpeedDial  
-        ariaLabel="SpeedDial"  
-        sx={{ position: 'absolute', bottom: 2, right: 2 }}  
-        icon={<SpeedDialIcon />}  
-        onClose={handleClose}  
-        onOpen={handleOpen}  
-        open={open}  
-      >  
-        {actions.map((action) => (  
-          <SpeedDialAction  
-            key={action.name}  
-            icon={action.icon}  
-            tooltipTitle={action.name}  
-            onClick={action.handler}  
-          />  
-        ))}  
-      </SpeedDial>  
-    </div>  
+  return (    
+    <div style={{ position: 'fixed', bottom: matches ? '16px' : 'unset', top: matches ? 'unset' : '14px', right: '14px', zIndex: 2000 }}>    
+      <SpeedDial    
+        ariaLabel="SpeedDial"    
+        sx={{ position: 'absolute', bottom: matches ? 2 : 'unset', top: matches ? 'unset' : 2, right: 2 }}    
+        icon={<SpeedDialIcon />}    
+        onClose={handleClose}    
+        onOpen={handleOpen}    
+        open={open}    
+        direction={matches ? 'up' : 'down'}
+      >    
+        {actions.map((action) => (    
+          <SpeedDialAction    
+            key={action.name}    
+            icon={action.icon}    
+            tooltipTitle={action.name}    
+            onClick={action.handler}    
+          />    
+        ))}    
+      </SpeedDial>    
+    </div>    
   );  
 };  
