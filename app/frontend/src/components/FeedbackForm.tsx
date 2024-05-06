0fefa9ddad0f21f469ffa9ba3d6b638ee037348e
kyleaitken <kyleaitken24@gmail.com>
2024-05-06 14:14:15 -0400

    remove un-used imports

--- app/frontend/src/components/FeedbackForm.tsx
+++ app/frontend/src/components/FeedbackForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useTranslation } from "react-i18next";
 import { Button, Dialog, DialogActions, DialogTitle, DialogContent, TextField, Typography } from '@mui/material';
 
