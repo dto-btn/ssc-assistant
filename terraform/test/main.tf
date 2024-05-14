3c372cc2cb9d9f1fa80173be23da46857e815e79
kyleaitken <kyleaitken24@gmail.com>
2024-05-14 10:31:52 -0400

    terraform

--- terraform/test/main.tf
+++
@@ -1,7 +0,0 @@
-/****************************************************
-*                       RG                          *
-*****************************************************/
-resource "azurerm_resource_group" "test" {
-  name     = "${var.name_prefix}${var.project_name}-rg"
-  location = var.default_location
-}
