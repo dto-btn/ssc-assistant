03d93d30e85e72f7fce73bfbb5d0065393d5dfb8
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-05-23 10:25:56 -0400

    wip: more changes

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -169,7 +169,7 @@ export const App = () => {
       messages: messages,
       max: maxMessagesSent,
       top: 5,
-      tools: ['corporate', 'geds', 'bits'],
+      tools: ["bits"],
       uuid: uuid,
     };
 
