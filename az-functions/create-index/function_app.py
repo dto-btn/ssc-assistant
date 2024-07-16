8ee718dba6a8837ffcf922dab51c10e6ccbafc2e
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-07-16 14:48:41 -0400

    fixed alias process (#148)
    [skip ci]

--- az-functions/create-index/function_app.py
+++ az-functions/create-index/function_app.py
@@ -1,10 +1,8 @@
 import logging
 import azure.functions as func
-from azure.functions import TimerRequest
 import azure.durable_functions as df
 from fetch_index_data_bp import fetch_data_bp
 from build_seach_index_bp import build_index_bp
-
 
 app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 app.register_functions(fetch_data_bp)
@@ -24,8 +22,6 @@ async def http_start(req: func.HttpRequest, client):
 @app.schedule(schedule="0 0 4 * * Fri", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 @app.durable_client_input(client_name="client")
 async def fetch_index_timer_trigger(myTimer: func.TimerRequest, client) -> None:
-    if myTimer.past_due:
-        logging.info("The timer is past due")
     instance_id = await client.start_new("fetch_index_data")
     logging.info("fetch index timer trigger function executed")
 
@@ -34,8 +30,12 @@ async def fetch_index_timer_trigger(myTimer: func.TimerRequest, client) -> None:
 @app.schedule(schedule="0 0 4 * * Sat", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 @app.durable_client_input(client_name="client")
 async def build_index_timer_trigger(myTimer: func.TimerRequest, client) -> None:
-    if myTimer.past_due:
-        logging.info("The timer is past due")
     instance_id = await client.start_new("build_search_index")
     logging.info("build index timer trigger function executed")
 
+# create alias for the newly built index.
+@app.schedule(schedule="0 0 8 * * Sat", arg_name="myTimer", run_on_startup=False, use_monitor=False)
+@app.durable_client_input(client_name="client")
+async def update_current_index_alias_trigger(myTimer: func.TimerRequest, client) -> None:
+    instance_id = await client.start_new("update_current_index_alias")
+    logging.info("update_current_index_alias function executed")
