0e0f6e0a44708c20111225e11d4c02e274b1b5c7
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-07-18 10:54:19 -0400

    wip: added merged translation files

--- app/frontend/src/i18n.ts
+++ app/frontend/src/i18n.ts
@@ -1,20 +1,38 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import en from './i18n/locales/en.json';
-import fr from './i18n/locales/fr.json';
+import translations from './i18n/locales/translations.json';
 
 import Cookies from "js-cookie";
+
+interface TranslationData {
+  [key: string]: {
+    en: string;
+    fr: string;
+  };
+}
+
+const extractLanguageData = (mergedData: TranslationData, language: keyof TranslationData[keyof TranslationData]): { [key: string]: string } => {
+  const languageData: { [key: string]: string } = {};
+  for (const [key, translations] of Object.entries(mergedData)) {
+    languageData[key] = translations[language] || 'MISSING';
+  }
+  return languageData;
+};
+
+// Extract English and French translations
+const enTranslations = extractLanguageData(translations, 'en');
+const frTranslations = extractLanguageData(translations, 'fr');
 
 i18n.use(initReactI18next).init({
   fallbackLng: 'en',
   lng: Cookies.get("lang_setting"),
   resources: {
     en: {
-      translations: en
+      translations: enTranslations
     },
     fr: {
-      translations: fr
+      translations: frTranslations
     }
   },
   ns: ['translations'],
