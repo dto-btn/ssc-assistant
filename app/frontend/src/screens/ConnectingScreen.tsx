d633adefd7bf37cd11d6c4ea1a59fbe79b4ffadb
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-07-24 14:11:00 -0400

    indent to 4 spaces

--- app/frontend/src/screens/ConnectingScreen.tsx
+++ app/frontend/src/screens/ConnectingScreen.tsx
@@ -5,17 +5,17 @@ const ConnectingScreen = () => {
     return (
         <ConnectingScreenView>
             <ConnectingContainer>
-            <img src={logo} style={{width: 'auto', height: '100px'}} alt="logo of SSC" />
-            <ConnectingTextWrapper>
-                <ConnectingText variant="h6" align="left">En cours de connection...</ConnectingText>
-                <ConnectingText variant="h6" align="left">Connecting...</ConnectingText>
-            </ConnectingTextWrapper>
+                <img src={logo} style={{width: 'auto', height: '100px'}} alt="logo of SSC" />
+                <ConnectingTextWrapper>
+                    <ConnectingText variant="h6" align="left">En cours de connection...</ConnectingText>
+                    <ConnectingText variant="h6" align="left">Connecting...</ConnectingText>
+                </ConnectingTextWrapper>
             </ConnectingContainer>
             <LoadingSpinnerView sx={{ display: 'flex', justifyContent: 'center', my: '2rem', marginTop: '100px' }}>
-            <CircularProgress
-                sx={{ color: 'url(#multicolor)' }}
-                size={50}
-            />
+                <CircularProgress
+                    sx={{ color: 'url(#multicolor)' }}
+                    size={50}
+                />
             </LoadingSpinnerView>
         </ConnectingScreenView>
     )
@@ -24,30 +24,30 @@ const ConnectingScreen = () => {
 export default ConnectingScreen;
 
 const ConnectingScreenView = styled(Box)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  height: 70vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 70vh;
 `;
 
 const ConnectingContainer = styled(Box)`
-  display: flex;
-  justify-content: center;
-  width: 25%;
-  padding: 50px;
+    display: flex;
+    justify-content: center;
+    width: 25%;
+    padding: 50px;
 `;
 
 const ConnectingTextWrapper = styled(Box)`
-  margin-left: 50px;
+    margin-left: 50px;
 `;
 
 const ConnectingText = styled(Typography)`
-  padding: 10px 0px;
+    padding: 10px 0px;
 `;
 
 const LoadingSpinnerView = styled(Box)`
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 `;
 
