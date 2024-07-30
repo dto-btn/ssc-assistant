749093822467d4a1899ebafcfab2897a4bf68b96
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-07-30 14:35:10 -0400

    wip: adding missing func app and ss log worskpace

--- terraform/search_services.tf
+++ terraform/search_services.tf
@@ -11,5 +11,14 @@ resource "azurerm_search_service" "main" {
   public_network_access_enabled = true
   local_authentication_enabled = true
   authentication_failure_mode = "http401WithBearerChallenge"
+}
 
+resource "azurerm_monitor_diagnostic_setting" "search_service_diagnostics" {
+  name                       = "${replace(var.project_name, "_", "-")}-search-diag"
+  target_resource_id         = azurerm_search_service.main.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+
+  enabled_log {
+    category = "OperationLogs"
+  }
 }
