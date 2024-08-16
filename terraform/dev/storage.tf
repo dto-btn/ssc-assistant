0bd92a6beb0c7dbce7354ff4eccaba9ff9732a22
kyleaitken <kyleaitken24@gmail.com>
2024-08-16 09:25:41 -0400

    update archibus storage container access level to blob

--- terraform/dev/storage.tf
+++ terraform/dev/storage.tf
@@ -27,6 +27,7 @@ resource "azurerm_storage_table" "flagged" {
 resource "azurerm_storage_container" "archibus" {
   name                 = "archibus-floor-plans"
   storage_account_name = azurerm_storage_account.dev.name
+  container_access_type = "blob"
 }
 
 data "azuread_user" "dev1" {
