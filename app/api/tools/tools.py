4b34d74aa05d1a2828f9c58a64a1396f2ec203d2
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-09-27 18:15:39 +0000

    wip: added missing hook for reflective function calls

--- app/api/tools/tools.py
+++ app/api/tools/tools.py
@@ -43,7 +43,8 @@ def discover_functions_with_metadata(base_path):
                         attr = getattr(module, attr_name)
                         if callable(attr) and hasattr(attr, "_tool_metadata"):
                             metadata = attr._tool_metadata
-                    functions_with_metadata[metadata["function"]["name"]] = {'metadata': metadata, 'module_name': module_name, 'tool_type': tool_type}
+                            metadata['tool_type'] = tool_type # also add tool type on top of the rest of things
+                    functions_with_metadata[metadata["function"]["name"]] = {'metadata': metadata, 'module': module, 'tool_type': tool_type}
     return functions_with_metadata
 
 def get_functions_with_metadata():
@@ -55,6 +56,11 @@ def get_functions_with_metadata():
     return _functions_with_metadata
 
 def load_tools(tools_requested: List[str]) -> List[ChatCompletionMessageParam]:
+    """
+    ONLY load tools if they are:
+        1) requested for (tools_requested) AND
+        2) part of the _allowed_tools list (set by the system)
+    """
     tools = []
     for _, value in get_functions_with_metadata().items():
         # Ensure BOTH function type is in requested types and ALLOWED types by the system.
@@ -66,42 +72,43 @@ def call_tools(tool_calls, messages: List[ChatCompletionMessageParam]) -> List[C
     """
     Call the tool functions and return a new completion with the results
     """
-    # Define the available functions
-    available_functions = {
-
-    }
 
     # Send the info for each function call and function response to the model
     for tool_call in tool_calls:
         function_name = tool_call.function.name
-        if function_name in available_functions:
-            function_to_call = available_functions[function_name]
-            function_args = json.loads(tool_call.function.arguments)
+        function_args = json.loads(tool_call.function.arguments)
 
-            # Prepare the arguments for the function call
-            prepared_args = {arg: function_args[arg] for arg in function_args}
+        # Prepare the arguments for the function call
+        prepared_args = {arg: function_args[arg] for arg in function_args}
 
-            # Call the function with the prepared arguments
+        # Call the function with the prepared arguments
+        try:
+            module = get_functions_with_metadata()[function_name]['module']
+            function_to_call = getattr(module, function_name)
             function_response = function_to_call(**prepared_args)
-
-            messages.append({
-                "role": "assistant",
-                "content": None,
-                "function_call": {
-                    "name": function_name,
-                    "arguments": json.dumps(function_args)
-                }
-            })
-
-            # Convert the function response to a string
-            response_as_string = "\n".join(function_response) if function_response is list else str(function_response) # type: ignore
-
-            # Add the function response to the messages
-            messages.append({
-                "role": "function",
+        except:
+            e = "Unable to call function"
+            logger.error(e)
+            function_response = e
+        
+        messages.append({
+            "role": "assistant",
+            "content": None,
+            "function_call": {
                 "name": function_name,
-                "content": response_as_string
-            })
-            # reworking with this example to refine a bit:
-            # https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/function-calling?tabs=python#working-with-function-calling
+                "arguments": json.dumps(function_args)
+            }
+        })
+
+        # Convert the function response to a string
+        response_as_string = "\n".join(function_response) if function_response is list else str(function_response) # type: ignore
+
+        # Add the function response to the messages
+        messages.append({
+            "role": "function",
+            "name": function_name,
+            "content": response_as_string
+        })
+        # reworking with this example to refine a bit:
+        # https://learn.microsoft.com/en-us/azure/ai-services/openai/how-to/function-calling?tabs=python#working-with-function-calling
     return messages
