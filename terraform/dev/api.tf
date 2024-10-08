377f56b65ace8b62449eafc1c404c73e5a370810
Sri Harsha Vardhan Kakumanu <harshakakumanu@users.noreply.github.com>
2024-10-08 12:43:26 -0600

    Origin/224 add alert (#225)
    * Added alert for prod for 5xx/4xx, sending emails when occuring.
    
    ---------
    
    Co-authored-by: Guillaume Turcotte <leguille.ftw@gmail.com>

--- terraform/dev/api.tf
+++ terraform/dev/api.tf
@@ -1,5 +1,5 @@
 /****************************************************
-*                 Azure App frontend                *
+*                 Azure App Dev API                *
 *****************************************************/
 resource "azurerm_service_plan" "api" {
   name                = "${var.name_prefix}${var.project_name}-api-plan"
