bc71181e101523530e9bb1dced9758de2dbce14e
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-10-10 12:46:32 +0000

    wip: started importing all the archibus code inside the API

---
+++ app/api/tools/archibus/archibus_functions.py
@@ -0,0 +1,197 @@
+from datetime import datetime
+import requests
+import os
+import json
+import logging
+
+from utils.decorators import tool_metadata
+
+logger = logging.getLogger(__name__)
+
+api_url = str(os.getenv("ARCHIBUS_API_USERNAME", "http://archibusapi-dev.hnfpejbvhhbqenhy.canadacentral.azurecontainer.io/api/v1"))
+api_username = str(os.getenv("ARCHIBUS_API_USERNAME"))
+api_password = str(os.getenv("ARCHIBUS_API_PASSWORD"))
+
+@tool_metadata({
+    "type": "function",
+    "tool_type": "archibus",
+    "function": {
+      "name": "get_user_bookings",
+      "description": "Gets a user's existing bookings in the archibus system by their first and last name. Do not use this method unless you have been asked to retrieve a user's bookings and have their first and last name",
+      "parameters": {
+        "type": "object",
+        "properties": {
+            "firstName": {
+                "type": "string",
+                "description": "A string indicating the first name of the user."
+            },
+            "lastName": {
+                "type": "string",
+                "description": "A string indicating the last name of the user."
+            }
+        },
+        "required": ["firstName", "lastName"]
+      }
+    }
+  })
+def get_user_bookings():
+    pass
+
+@tool_metadata({
+    "type": "function",
+    "tool_type": "archibus",
+    "function": {
+      "name": "get_floors",
+      "description": "Gets a list of the available floors in a building where a user can make a booking. If the user is attempting to make a booking and does not specify a floor, use this method to return a list of available floors to them and ask which floor they would like to book on before proceeding to any other functions. Do not use this method unless you have a buildingId. DO NOT USE A BUILDING NAME OR ADDRESS IN PLACE OF AN ID. Return the buildingId as well when you answer so you have it for later.",
+      "parameters": {
+        "type": "object",
+        "properties": {
+            "buildingId": {
+                "type": "string",
+                "description": "The unique identifier of the building where the booking takes place. Do not use the building name or address as the id"
+            }
+        },
+        "required": ["buildingId"]
+      }
+    }
+  })
+def get_floors(buildingId: str):
+    try:
+        uri = f"/buildings/{buildingId}/floors"
+        response = _make_api_call(uri)
+        response_json = json.loads(response.text)
+        pretty_response = json.dumps(response_json, indent=4)
+        logger.debug(pretty_response)
+        logger.debug(f"Response status code: {response.status_code}")
+        return response.json()  
+    except requests.RequestException as e:
+        msg = f"An error occurred while trying to fetch floors for the building {buildingId}."
+        logger.error(msg)
+        return msg
+
+@tool_metadata({
+    "type": "function",
+    "tool_type": "archibus",
+    "function": {
+      "name": "get_available_rooms",
+      "description": "Gets a list of all the vacant rooms or workspaces in a specified building and floor. Use this method once you have a buildingId, floorId, and date to retrieve a list of all the vacant rooms. IF YOU DONT HAVE A BUILDINGID, USE GET_BUILDINGS FUNCTION FIRST. DO NOT USE THE BUILDING ADDRESS OR NAME AS THE BUILDINGID. You should then present this list of rooms to the user and ask which room they would like to book.",
+      "parameters": {
+        "type": "object",
+        "properties": {
+          "buildingId": {
+            "type": "string",
+            "description": "A string indicating the ID of the building."
+          },
+          "floorId": {
+            "type": "string",
+            "description": "A string indicating the ID of the floor within the specified building."
+          },
+          "bookingDate": {
+            "type": "string",
+            "description": "A string indicating the date of the booking, including the month, day, and year, formatted like YYYY-MM-DD. The default year is 2024."
+          }
+        },
+        "required": ["buildingId", "floorId", "bookingDate"]
+      }
+    }
+  })
+def get_available_rooms(buildingId: str, floorId: str, bookingDate: str):
+    floor_plan_file_name = get_floor_plan(buildingId=buildingId, floorId=floorId)
+    logger.debug(f"FILE NAME: {floor_plan_file_name}")
+
+    try:
+        uri = f"/reservations/buildings/{buildingId}/vacant/{floorId}?bookingDate={bookingDate}"
+        response = _make_api_call(uri)
+        response_json = json.loads(response.text)
+        filtered_rooms = response_json[:10]
+        pretty_response = json.dumps(filtered_rooms, indent=4)
+
+        logger.debug(pretty_response)
+        logger.debug(f"Response status code: {response.status_code}")
+
+        result = {
+            "rooms": filtered_rooms
+        }
+
+        if floor_plan_file_name is not None:
+            # base64_svg = base64.b64encode(floor_plan_blob).decode('utf-8')
+            result["floorPlan"] = floor_plan_file_name
+
+        return result
+    except requests.HTTPError as e:
+        msg = f"An error occurred while trying to fetch rooms for the given floor {floorId} and building {buildingId}."
+        logger.error(msg)
+        return msg
+
+
+@tool_metadata({
+    "type": "function",
+    "tool_type": "archibus",
+    "function": {
+      "name": "get_floor_plan",
+      "description": "Retrieves the floor plan image associated with the selected floor from the user.",
+      "parameters": {
+        "type": "object",
+        "properties": {
+            "buildingId": {
+            "type": "string",
+            "description": "A string indicating the ID of the building."
+            },
+            "floorId": {
+              "type": "string",
+              "description": "A string indicating the ID of the floor within the specified building."
+            }
+        },
+        "required": ["floorId", "buildingId"]
+      }
+    }
+  })
+def get_floor_plan(buildingId: str, floorId: str):
+    try:
+        uri = f"/buildings/{buildingId}/floors"
+        response = _make_api_call(uri)
+        response_json = json.loads(response.text)
+        target_floor_blob_name = None
+
+        for floor in response_json:
+            if floor.get('flId') == floorId:
+                target_floor_blob_name = floor.get('floorPlanURL')
+                break
+
+        if target_floor_blob_name:
+            return target_floor_blob_name
+        else:
+            logger.error(f"Floor plan URL not found for floorId: {floorId}")
+            return None
+
+    except requests.HTTPError as e:
+        msg = f"Error occurred during the request to retrieve floors: {e}"
+        logger.error(msg)
+        return msg
+
+@tool_metadata({
+    "type": "function",
+    "tool_type": "archibus",
+    "function": {
+      "name": "get_current_date",
+      "description": "This function is used to know what is the current date and time. It returns the current date and time in text format. Use this if you are unsure of what is the current date, do not make assumptions about the current date and time."
+    }
+  })
+def get_current_date() -> str:
+    """
+    TODO: this perhaps should be moved into a more generic tools folder.
+    """
+    current_date_time = datetime.now()
+    return "Formatted date and time:" + current_date_time.strftime("%Y-%m-%d %H:%M:%S")
+
+def _make_api_call(uri: str, ) -> requests.Response:
+    auth = (api_username, api_password)
+
+    headers = {
+        'Accept': 'application/json'
+    }
+
+    response = requests.get(api_url + uri, headers=headers, auth=auth)
+    logger.debug(api_url + uri)
+    response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+    return response
