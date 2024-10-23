75bcdb28119181fac0be1dc35a4da3f2f3a7186f
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-10-23 10:02:57 -0400

    wip: added access for cody and updated example env file for  more current representation of params needed

--- terraform/dev/access.tf
+++ terraform/dev/access.tf
@@ -9,18 +9,34 @@ data "azuread_user" "po-af" {
   user_principal_name = "alain.forcier@ssc-spc.gc.ca"
 }
 
+data "azuread_user" "codyrobillard" {
+  user_principal_name = "cody.robillard@ssc-spc.gc.ca"
+}
+
 data "azuread_service_principal" "terraform" {
   display_name = "Terraform-CIO-Automation-SP"
 }
 
 #######################################################
-#                   Sub Contributors                  #
+#             Sub Contributors/Reader                 #
 #######################################################
 # resource "azurerm_role_assignment" "sub-contri-1" {
 #   scope                = azurerm_resource_group.dev.id
 #   role_definition_name = "Contributor"
 #   principal_id         = ###
 # }
+
+#Read Sub and Use OpenAI services
+resource "azurerm_role_assignment" "sub-read-cody" {
+  scope                = azurerm_resource_group.dev.id
+  role_definition_name = "Reader"
+  principal_id         = data.azuread_user.codyrobillard.id
+}
+resource "azurerm_role_assignment" "openai_user_cody" {
+  role_definition_name = "Cognitive Services User"
+  scope = data.azurerm_resource_group.ai.id
+  principal_id = data.azuread_user.codyrobillard.id
+}
 
 #######################################################
 #                      APPS                           #
