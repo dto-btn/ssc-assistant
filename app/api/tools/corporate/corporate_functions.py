edc4c0e3a4ccac4813baf07ebfcef455d669358d
Guillaume Turcotte <leguille.ftw@gmail.com>
2024-12-04 10:55:00 -0500

    wip:added translation and function for pmcoe data

--- app/api/tools/corporate/corporate_functions.py
+++ app/api/tools/corporate/corporate_functions.py
@@ -55,3 +55,24 @@ def intranet_question(*args): # pylint: disable=unused-argument
 def telecom_question(*args): # pylint: disable=unused-argument
     """returns the name of the telecom index name"""
     return "ds-tbssn-sat"
+
+@tool_metadata({
+    "type": "function",
+    "function": {
+        "name": "pmcoe_question",
+        "description": "The library contains bilingual gate templates (French and English), as well as Shared Services Canada (SSC) project management artifacts and standardized templates to support consistent project delivery and documentation.",
+        "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+            "type": "string",
+            "description": "The question that relates to anything related to project managment or gate templates within SSC"
+            }
+        },
+        "required": ["query"]
+        }
+    }
+})
+def pmcoe_question(*args): # pylint: disable=unused-argument
+    """returns the name of the telecom index name"""
+    return "pmcoe-cegp"
