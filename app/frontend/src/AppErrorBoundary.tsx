7f12156e3d0eb599d24b69a1b6df9975a5c8c439
Monarch Wadia <monarchwadia@gmail.com>
2025-01-07 14:27:07 -0500

    Removed unused declarations.

--- app/frontend/src/AppErrorBoundary.tsx
+++ app/frontend/src/AppErrorBoundary.tsx
@@ -29,12 +29,3 @@ export const AppErrorBoundary: React.FC<PropsWithChildren> = ({ children }) => {
         </ErrorBoundary>
     );
 }
-
-const FallbackContainer = styled('div')({
-    padding: '20px',
-    textAlign: 'center',
-    backgroundColor: '#f8d7da',
-    color: '#721c24',
-    border: '1px solid #f5c6cb',
-    borderRadius: '4px'
-});
