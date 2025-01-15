cc9d5483533762a781dfd1ba45bc290ad7ab9a23
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-01-15 14:20:05 -0500

    fix for #282 convo

--- app/frontend/src/components/UploadFileButton.tsx
+++ app/frontend/src/components/UploadFileButton.tsx
@@ -80,7 +80,9 @@ export default function InputFileUpload({
       <VisuallyHiddenInput
         type="file"
         onChange={encodeAndUploadFile}
-        accept=".jpg,.jpeg,.png,.gif,.png,.webp,.bmp,.svg" //only images for now since OpenAI API only supports images
+        accept=".jpg,.jpeg,.png,.webp" //only images for now since OpenAI API only supports images
+        // https://platform.openai.com/docs/guides/vision#what-type-of-files-can-i-upload (TLDR; png, jpeg, jpg, webp.)
+        // (and non animated gif but we will omit them for simplicity for now)
         //accept=".jpg,.jpeg,.png,.pdf,.doc,.docx,.xls,.xlsx,.ppt,.pptx,.txt,.csv"
         //multiple
       />
