19518d722c8240a3321e9940ea5c63badebecd4e
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-01-30 13:23:09 -0500

    wip: testing commit for disabled features

--- app/frontend/src/allowedTools.ts
+++ app/frontend/src/allowedTools.ts
@@ -5,3 +5,7 @@ export const allowedToolsSet = new Set<string>(allowedToolsArray);
 export const allowedCorporateFunctionsString = import.meta.env.VITE_ALLOWED_CORPORATE_FUNCTIONS || '';
 export const allowedCorporateFunctionsArray = allowedCorporateFunctionsString.split(',').map((tool: string) => tool.trim());
 export const allowedCorporateFunctionsSet = new Set<string>(allowedCorporateFunctionsArray);
+
+export const disabledFeaturesString = import.meta.env.VITE_DISABLED_FEATURES || '';
+export const disabledFeaturesArray = disabledFeaturesString.split(',').map((tool: string) => tool.trim());
+export const disabledFeaturesSet = new Set<string>(disabledFeaturesArray);
