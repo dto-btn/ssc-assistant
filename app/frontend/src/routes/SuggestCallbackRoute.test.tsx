ac7ba017a7c122d485cc1aeea0631a47b50c4569
Monarch Wadia <monarchwadia@gmail.com>
2025-02-17 12:19:00 -0500

    Removed unnecessary imports in suggestcallbackroute.test

--- app/frontend/src/routes/SuggestCallbackRoute.test.tsx
+++ app/frontend/src/routes/SuggestCallbackRoute.test.tsx
@@ -1,8 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { SuggestCallbackRoute } from './SuggestCallbackRoute';
-import 'jsdom';
 
 describe('SuggestCallbackRoute', () => {
     it('should render the Suggest Callback route', async () => {
