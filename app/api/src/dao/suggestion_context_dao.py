8102abd05381c40426bee0d9878e879f824ead3c
Monarch Wadia <monarchwadia@gmail.com>
2025-02-28 10:39:42 -0500

    Added delete before logic

--- app/api/src/dao/suggestion_context_dao.py
+++ app/api/src/dao/suggestion_context_dao.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 from src.entity.suggestion_context_entity import SuggestionContextEntity
 
@@ -15,3 +16,17 @@ class SuggestionContextDao:
             (suggestion for suggestion in self._suggestions if suggestion.id == id),
             None,
         )
+
+    def insert_suggestion_context(self, suggestion: SuggestionContextEntity):
+        self._suggestions.append(suggestion)
+
+    def delete_suggestion_context_older_than(self, delete_before_inclusive: datetime):
+        """
+        Deletes all suggestions older than delete_before.
+        The cutoff is inclusive, so suggestions with a timestamp equal to the cutoff will also be deleted.
+        """
+        self._suggestions = [
+            suggestion
+            for suggestion in self._suggestions
+            if suggestion.timestamp > delete_before_inclusive
+        ]
--- app/api/src/dao/test_suggestion_context_dao.py
+++ app/api/src/dao/test_suggestion_context_dao.py
@@ -1,7 +1,37 @@
 from datetime import datetime
 import pytest
 from src.dao.suggestion_context_dao import SuggestionContextDao
-from src.entity.suggestion_context_entity import SuggestionContextEntity
+from src.entity.suggestion_context_entity import (
+    SuggestionContextCitationEntity,
+    SuggestionContextEntity,
+)
+from uuid import uuid4
+
+
+def build_mock_suggestion_context_entity():
+    uuid = str(uuid4())
+
+    citation_1 = SuggestionContextCitationEntity(
+        title="Some citation",
+        url="https://example.com/some-citation",
+    )
+    citation_2 = SuggestionContextCitationEntity(
+        title="Another citation",
+        url="https://example.com/another-citation",
+    )
+    return SuggestionContextEntity(
+        id=uuid,
+        citations=[
+            citation_1,
+            citation_2,
+        ],
+        content="this is a test suggestion",
+        language="en",
+        original_query="user's query",
+        requester="test",
+        success=True,
+        timestamp=datetime.now(),
+    )
 
 
 @pytest.fixture(scope="function")
@@ -28,19 +58,48 @@ def test_get_suggestion_context_by_id_returns_suggestion(
     suggestion_context_dao: SuggestionContextDao,
 ):
     TEST_ID = "this-id-will-exist-in-db"
-    suggestion_context_dao._suggestions = [
-        SuggestionContextEntity(
-            id=TEST_ID,
-            citations=[],
-            content="this is a test suggestion",
-            language="en",
-            original_query="user's query",
-            requester="test",
-            success=True,
-            timestamp=datetime.now(),
-        )
-    ]
+    suggestion = build_mock_suggestion_context_entity()
+    suggestion.id = TEST_ID
+    suggestion_context_dao._suggestions = [suggestion]
     suggestions = suggestion_context_dao.get_suggestion_context_by_id(TEST_ID)
     assert suggestions is not None
     assert suggestions.id == TEST_ID
-    assert suggestions.content == "this is a test suggestion"
+
+# can insert new suggestion
+
+
+def test_insert_suggestion_context_inserts_suggestion(
+    suggestion_context_dao: SuggestionContextDao,
+):
+    suggestion = build_mock_suggestion_context_entity()
+    suggestion_context_dao.insert_suggestion_context(suggestion)
+    assert len(suggestion_context_dao._suggestions) == 1
+    assert suggestion_context_dao._suggestions[0] == suggestion
+
+
+# can delete suggestion by oldest_timestamp
+# def delete_suggestion_context_older_than(self, oldest_timestamp: datetime):
+
+
+def test_delete_suggestion_context_older_than_deletes_suggestions(
+    suggestion_context_dao: SuggestionContextDao,
+):
+    suggestion1 = build_mock_suggestion_context_entity()
+    suggestion1.timestamp = datetime(2021, 1, 1)  # will be gone
+    suggestion2 = build_mock_suggestion_context_entity()
+    suggestion2.timestamp = datetime(2021, 1, 2)  # will be gone
+    suggestion3 = build_mock_suggestion_context_entity()
+    suggestion3.timestamp = datetime(2021, 1, 3)
+    suggestion4 = build_mock_suggestion_context_entity()
+    suggestion4.timestamp = datetime(2021, 1, 4)
+
+    suggestion_context_dao._suggestions = [
+        suggestion1,
+        suggestion2,
+        suggestion3,
+        suggestion4,
+    ]
+    suggestion_context_dao.delete_suggestion_context_older_than(datetime(2021, 1, 2))
+    assert len(suggestion_context_dao._suggestions) == 2
+    assert suggestion3 in suggestion_context_dao._suggestions
+    assert suggestion4 in suggestion_context_dao._suggestions
