a51ae3ef4906bd7267f879401c34e9e10ced8bc5
Monarch Wadia <monarchwadia@gmail.com>
2025-03-18 07:51:39 -0400

    Removed pointer cursor from SSC Assistant logo. Deleted unused old TopMenu file.

--- app/frontend/src/components/TopMenu/subcomponents/TopMenuFrame.tsx
+++ app/frontend/src/components/TopMenu/subcomponents/TopMenuFrame.tsx
@@ -42,7 +42,6 @@ export const TopMenuFrame: FC<TopMenuProps> = (({ children }) => {
                         display: "flex",
                         alignItems: "center",
                         gap: "1rem",
-                        cursor: "pointer",
                         userSelect: "none",
                     }}>
                         <img src={logo} style={{
