2a290bb60a988ffe46ea749d78d6acc15f601046
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-03-20 09:39:18 -0400

    fixed conn str value

--- terraform/modules/db/output.tf
+++ terraform/modules/db/output.tf
@@ -1,5 +1,5 @@
 output "postgres_connection_string" {
-  value = "postgresql://${var.username_postgress}:${var.password_postgress}@${azurerm_postgresql_flexible_server.db.name}.postgres.database.azure.com:5432/${var.db_name}"
+  value = "postgresql+psycopg://${var.username_postgress}:${var.password_postgress}@${azurerm_postgresql_flexible_server.db.name}.postgres.database.azure.com:5432/${var.db_name}"
   description = "The PostgreSQL connection string"
   sensitive = true
 }
