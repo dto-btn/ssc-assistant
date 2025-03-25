309430c5ed953cab54a6bc95589526af4978274c
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-03-25 14:44:53 -0400

    wip; added filter by status query

--- app/api/tools/bits/bits_functions.py
+++ app/api/tools/bits/bits_functions.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-from datetime import datetime
 
 from src.constants.tools import TOOL_BR
 from tools.bits.bits_utils import DatabaseConnection, extract_fields_from_query
@@ -50,151 +49,10 @@ valid_search_fields = [
 def get_br_information(br_numbers: list[int]):
     """
     gets br information
-
-    SELECT
-        br.BR_NMBR,
-        br.BR_TITLE,
-        br.BR_SHORT_TITLE,
-        s.BR_ACTIVE_EN,
-        s.BR_ACTIVE_FR,
-        s.BITS_STATUS_EN,
-        s.BITS_STATUS_FR
-    FROM
-        [EDR_CARZ].[DIM_DEMAND_BR_ITEMS] br
-    INNER JOIN
-        (SELECT BR_NMBR, STATUS_ID
-        FROM [EDR_CARZ].[FCT_DEMAND_BR_SNAPSHOT]
-        WHERE PERIOD_END_DATE > '2025-03-24') snp
-    ON snp.BR_NMBR = br.BR_NMBR
-    INNER JOIN
-        [EDR_CARZ].[DIM_BITS_STATUS] s
-    ON s.STATUS_ID = snp.STATUS_ID
-    LEFT JOIN
-        [EDR_CARZ].[DIM_GC_ORGANIZATION] org
-    ON br.GC_ORG_NMBR = org.GC_ORG_NMBR
-    WHERE
-        br.BR_NMBR = 74096;
-
-    NOTE: No need to join on ORGANIZATION Table atm.. some info is already rolled in BR ITEMS ...
     """
-    placeholders = ", ".join(["%s"] * len(br_numbers))
-
-    today = datetime.today()
-    today_formatted = today.strftime('%Y-%m-%d')
-
-    query = f"""
-    SELECT
-        br.BR_NMBR,
-        br.BR_TITLE,
-        br.BR_SHORT_TITLE,
-        br.RPT_GC_ORG_NAME_EN,
-        br.RPT_GC_ORG_NAME_FR,
-        br.ORG_TYPE_EN,
-        br.ORG_TYPE_FR,
-        br.REQST_IMPL_DATE,
-        br.BR_TYPE_EN,
-        br.BR_TYPE_FR,
-        br.PRIORITY_EN,
-        br.PRIORITY_FR,
-        br.BR_OWNER,
-        br.SUBMIT_DATE,
-        br.RVSD_TARGET_IMPL_DATE,
-        br.BA_OPI,
-        br.CPLX_EN,
-        br.CPLX_FR,
-        br.ACTUAL_IMPL_DATE,
-        br.AGRMT_END_DATE,
-        br.BA_TL,
-        br.TL_OPI,
-        br.SOL_OPI,
-        br.PM_OPI,
-        br.SCOPE_EN,
-        br.SCOPE_FR,
-        br.CSM_DIRTR,
-        br.CLIENT_REQST_SOL_DATE,
-        br.BA_PRICE_OPI,
-        br.SDM_TL_OPI,
-        br.CLIENT_SUBGRP_EN,
-        br.CLIENT_SUBGRP_FR,
-        br.PROD_OPI,
-        br.PRPO_TARGET_DATE,
-        br.IMPL_SGNOFF_DATE,
-        br.GROUP_EN,
-        br.GROUP_FR,
-        br.ENGN_OPI,
-        br.AGRMT_OPI,
-        br.BA_PRICE_OPI,
-        br.ASSOC_BRs,
-        br.REQMT_OVRVW,
-        s.BR_ACTIVE_EN,
-        s.BR_ACTIVE_FR,
-        s.BITS_STATUS_EN,
-        s.BITS_STATUS_FR
-    FROM
-        [EDR_CARZ].[DIM_DEMAND_BR_ITEMS] br
-    INNER JOIN
-        (SELECT BR_NMBR, STATUS_ID
-        FROM [EDR_CARZ].[FCT_DEMAND_BR_SNAPSHOT]
-        WHERE PERIOD_END_DATE > '{today_formatted}') snp
-    ON snp.BR_NMBR = br.BR_NMBR
-    INNER JOIN
-        [EDR_CARZ].[DIM_BITS_STATUS] s
-    ON s.STATUS_ID = snp.STATUS_ID
-    WHERE
-        br.BR_NMBR IN ({placeholders});
-    """
-
+    query = _get_br_query(br_numbers)
     result = db.execute_query(query, *br_numbers)
     return {'br': result}
-
-# @tool_metadata({
-#     "type": "function",
-#     "function": {
-#         "name": "get_br_updates",
-#         "description": "Gets BR updates from the snapshot table.",
-#         "parameters": {
-#             "type": "object",
-#             "properties": {
-#                 "br_number": {
-#                     "type": "integer",
-#                     "description": "A BR number."
-#                 }
-#             },
-#             "required": ["br_number"]
-#       }
-#     }
-#   })
-# def get_br_updates(br_number: int):
-#     """
-#     gets br updates from snapshot table
-#     """
-#     query = """
-#     SELECT
-#         TOP(1)
-#         f.BR_NMBR,
-#         f.PERIOD_END_DATE,
-#         f.DAYS_SINCE_SUBMIT,
-#         f.LAST_STATUS_DATE,
-#         f.DAYS_IN_STATUS,
-#         f.AGE_IN_STATUS_EN,
-#         f.AGE_IN_STATUS_FR,
-#         f.IMPL_FLAG_EN,
-#         f.IMPL_FLAG_FR,
-#         d.BITS_STATUS_EN,
-#         d.BITS_STATUS_FR,
-#         d.BR_ACTIVE_EN,
-#         d.BR_ACTIVE_FR
-#     FROM
-#         [EDR_CARZ].[FCT_DEMAND_BR_SNAPSHOT] f
-#     INNER JOIN
-#         [EDR_CARZ].[DIM_BITS_STATUS] d
-#     ON
-#         f.STATUS_ID = d.STATUS_ID
-#     WHERE
-#         f.BR_NMBR = %s
-#      ORDER BY f.PERIOD_END_DATE DESC;"""
-#     result = db.execute_query(query, br_number)
-#     return {'br_updates': result}
 
 @tool_metadata({
     "type": "function",
@@ -210,7 +68,7 @@ def get_br_information(br_numbers: list[int]):
                 },
                 "limit": {
                     "type": "integer",
-                    "description": "The maximum number of BR items to return. Defaults to 15.",
+                    "description": "The maximum number of BR items to return. Defaults to 10.",
                     "default": 10
                 },
                 "assigned_to_fields": {
@@ -325,3 +183,151 @@ def how_to_search_brs():
         metadata[key] = value['metadata']
     print(metadata)
     return json.dumps(metadata)
+
+@tool_metadata({
+    "type": "function",
+    "function": {
+        "name": "get_br_statuses",
+        "description": "Use this function to list all the BR Statuses. This can be used to get the STATUS_ID. To perform search in other queries.",
+        "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+      }
+    }
+  })
+def get_br_statuses():
+    """
+    This will retreive the code table BR_STATUSES
+    """
+    query = "SELECT * FROM [EDR_CARZ].[DIM_BITS_STATUS];"
+    result = db.execute_query(query)
+    return {'br_statuses': result}
+
+@tool_metadata({
+    "type": "function",
+    "function": {
+        "name": "get_br_by_status",
+        "description": "Use this function to list all the BR filtered by a specific status.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "status": {
+                    "type": "string",
+                    "description": "This is the status (STATUS_ID) that the user wants to filter the requests for. If you don't know the statuses beforehand call get_br_statuses() function to get STATUS_ID."
+                },
+                "limit": {
+                    "type": "integer",
+                    "description": "The maximum number of BR items to return. Defaults to 100.",
+                    "default": 100
+                },
+                "assigned_to": {
+                    "type": "string",
+                    "description": "This is the name of the person that the BR is assigned to (BR_OWNER)"
+                }
+            },
+            "required": ["status"]
+      }
+    }
+  })
+def get_br_by_status(status: str, assigned_to: str = "", limit: int = 100):
+    """
+    This will retreive the code table BR_STATUSES
+    """
+    query = _get_br_query(status=status, limit=limit)
+    result = db.execute_query(query, limit, status)
+    return {'br': result}
+
+def _get_br_query(br_numbers: list[int] = [], status: str = "", limit: int = 0) -> str:
+    """Function that will build the select statement for retreiving BRs
+
+    NOTE: No need to join on ORGANIZATION Table atm.. some info is already rolled in BR ITEMS ...
+    """
+    if br_numbers is None:
+        br_numbers = []
+    if limit:
+        base_query = "SELECT TOP(%d) "
+    else:
+        base_query = "SELECT "
+    base_query += """
+        br.BR_NMBR,
+        br.BR_TITLE,
+        br.BR_SHORT_TITLE,
+        br.RPT_GC_ORG_NAME_EN,
+        br.RPT_GC_ORG_NAME_FR,
+        br.ORG_TYPE_EN,
+        br.ORG_TYPE_FR,
+        br.REQST_IMPL_DATE,
+        br.BR_TYPE_EN,
+        br.BR_TYPE_FR,
+        br.PRIORITY_EN,
+        br.PRIORITY_FR,
+        br.BR_OWNER,
+        br.SUBMIT_DATE,
+        br.RVSD_TARGET_IMPL_DATE,
+        br.BA_OPI,
+        br.CPLX_EN,
+        br.CPLX_FR,
+        br.ACTUAL_IMPL_DATE,
+        br.AGRMT_END_DATE,
+        br.BA_TL,
+        br.TL_OPI,
+        br.SOL_OPI,
+        br.PM_OPI,
+        br.SCOPE_EN,
+        br.SCOPE_FR,
+        br.CSM_DIRTR,
+        br.CLIENT_REQST_SOL_DATE,
+        br.BA_PRICE_OPI,
+        br.SDM_TL_OPI,
+        br.CLIENT_SUBGRP_EN,
+        br.CLIENT_SUBGRP_FR,
+        br.PROD_OPI,
+        br.PRPO_TARGET_DATE,
+        br.IMPL_SGNOFF_DATE,
+        br.GROUP_EN,
+        br.GROUP_FR,
+        br.ENGN_OPI,
+        br.AGRMT_OPI,
+        br.BA_PRICE_OPI,
+        br.ASSOC_BRs,
+        br.REQMT_OVRVW,
+        s.BR_ACTIVE_EN,
+        s.BR_ACTIVE_FR,
+        s.BITS_STATUS_EN,
+        s.BITS_STATUS_FR
+    FROM
+        [EDR_CARZ].[DIM_DEMAND_BR_ITEMS] br
+    """
+
+    if status:
+        base_query += """
+            INNER JOIN
+                (SELECT BR_NMBR, STATUS_ID
+                FROM [EDR_CARZ].[FCT_DEMAND_BR_SNAPSHOT]
+                WHERE PERIOD_END_DATE > GETDATE() AND STATUS_ID IN (%s)) snp
+            ON snp.BR_NMBR = br.BR_NMBR
+        """
+    else:
+        base_query += """
+            INNER JOIN
+                (SELECT BR_NMBR, STATUS_ID
+                FROM [EDR_CARZ].[FCT_DEMAND_BR_SNAPSHOT]
+                WHERE PERIOD_END_DATE > GETDATE()) snp
+            ON snp.BR_NMBR = br.BR_NMBR
+        """
+
+    base_query += """
+        INNER JOIN
+            [EDR_CARZ].[DIM_BITS_STATUS] s
+        ON s.STATUS_ID = snp.STATUS_ID
+    """
+
+    if br_numbers:
+        # Prevents SQL injection, this only calculates the placehoders ... i.e; BR_NMBR IN (%s, %s, %s)
+        placeholders = ", ".join(["%s"] * len(br_numbers))
+        base_query += f"""
+            WHERE
+            br.BR_NMBR IN ({placeholders});"
+        """
+    return base_query
