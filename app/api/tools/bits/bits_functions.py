7ba73537cd62482ea52115a30c1bacc41452f4a9
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-03-28 15:00:45 -0400

    wip: huge refactor again to allow for more dynamic filtering and easier way to maintain fields///

--- app/api/tools/bits/bits_functions.py
+++ app/api/tools/bits/bits_functions.py
@@ -16,16 +16,52 @@ db = DatabaseConnection(os.getenv("BITS_DB_SERVER", "missing.domain"),
                         os.getenv("BITS_DB_PWD", "missing.password"),
                         os.getenv("BITS_DB_DATABASE", "missing.dbname"))
 
-valid_assigned_to_fields = [
-        'BR_OWNER', 'BR_INITR', 'BR_LAST_EDITOR', 'CSM_OPI', 'TL_OPI', 'CSM_DIRTR', 'SOL_OPI',
-        'ENGN_OPI', 'BA_OPI', 'BA_TL', 'PM_OPI', 'BA_PRICE_OPI', 'QA_OPI', 'SL_COORD', 'AGRMT_OPI',
-        'ACCT_MGR_OPI', 'SDM_TL_OPI'
-    ]
-
-valid_search_fields = [
-    'BR_TITLE', 'BR_SHORT_TITLE', 'PRIORITY_EN', 'PRIORITY_FR', 'CLIENT_NAME_SRC',
-    'RPT_GC_ORG_NAME_EN', 'RPT_GC_ORG_NAME_FR', 'GROUP_ID', 'GROUP_EN', 'GROUP_FR'
-]
+valid_search_fields = {
+    #'BR_TITLE': 'br.BR_TITLE',
+    'BR_SHORT_TITLE': 'br.BR_SHORT_TITLE',
+    'RPT_GC_ORG_NAME_EN': 'br.RPT_GC_ORG_NAME_EN',
+    'RPT_GC_ORG_NAME_FR': 'br.RPT_GC_ORG_NAME_FR',
+    'ORG_TYPE_EN': 'br.ORG_TYPE_EN',
+    'ORG_TYPE_FR': 'br.ORG_TYPE_FR',
+    'REQST_IMPL_DATE': 'br.REQST_IMPL_DATE',
+    'BR_TYPE_EN': 'br.BR_TYPE_EN',
+    'BR_TYPE_FR': 'br.BR_TYPE_FR',
+    'PRIORITY_EN': 'br.PRIORITY_EN',
+    'PRIORITY_FR': 'br.PRIORITY_FR',
+    'BR_OWNER': 'br.BR_OWNER',
+    'SUBMIT_DATE': 'br.SUBMIT_DATE',
+    'RVSD_TARGET_IMPL_DATE': 'br.RVSD_TARGET_IMPL_DATE',
+    'BA_OPI': 'br.BA_OPI',
+    'CPLX_EN': 'br.CPLX_EN',
+    'CPLX_FR': 'br.CPLX_FR',
+    'ACTUAL_IMPL_DATE': 'br.ACTUAL_IMPL_DATE',
+    'AGRMT_END_DATE': 'br.AGRMT_END_DATE',
+    'BA_TL': 'br.BA_TL',
+    'TL_OPI': 'br.TL_OPI',
+    'SOL_OPI': 'br.SOL_OPI',
+    'PM_OPI': 'br.PM_OPI',
+    'SCOPE_EN': 'br.SCOPE_EN',
+    'SCOPE_FR': 'br.SCOPE_FR',
+    'CSM_DIRTR': 'br.CSM_DIRTR',
+    'CLIENT_REQST_SOL_DATE': 'br.CLIENT_REQST_SOL_DATE',
+    'BA_PRICE_OPI': 'br.BA_PRICE_OPI',
+    'SDM_TL_OPI': 'br.SDM_TL_OPI',
+    'CLIENT_SUBGRP_EN': 'br.CLIENT_SUBGRP_EN',
+    'CLIENT_SUBGRP_FR': 'br.CLIENT_SUBGRP_FR',
+    'PROD_OPI': 'br.PROD_OPI',
+    'PRPO_TARGET_DATE': 'br.PRPO_TARGET_DATE',
+    'IMPL_SGNOFF_DATE': 'br.IMPL_SGNOFF_DATE',
+    'GROUP_EN': 'br.GROUP_EN',
+    'GROUP_FR': 'br.GROUP_FR',
+    'ENGN_OPI': 'br.ENGN_OPI',
+    'AGRMT_OPI': 'br.AGRMT_OPI',
+    'ASSOC_BRS': 'br.ASSOC_BRS',
+    'REQMT_OVRVW': 'br.REQMT_OVRVW',
+    'BR_ACTIVE_EN': 's.BR_ACTIVE_EN',
+    'BR_ACTIVE_FR': 's.BR_ACTIVE_FR',
+    'BITS_STATUS_EN': 's.BITS_STATUS_EN',
+    'BITS_STATUS_FR': 's.BITS_STATUS_FR'
+}
 
 @tool_metadata({
     "type": "function",
@@ -55,68 +91,68 @@ def get_br_information(br_numbers: list[int]):
     result = db.execute_query(query, *br_numbers)
     return {'br': result}
 
-@tool_metadata({
-    "type": "function",
-    "function": {
-        "name": "get_br_assigned_to",
-        "description": "Gets information about BRs assigned to a given name. If no limit is specified, it returns the top 10 items by default. List of fields a user can specify in assigned_to_fields: {fields}".replace("{fields}", ", ".join(valid_assigned_to_fields)),
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "name": {
-                    "type": "string",
-                    "description": "The name to search for in the BR records. This can be the BR owner, initiator, last editor, or other specified roles."
-                },
-                "limit": {
-                    "type": "integer",
-                    "description": "The maximum number of BR items to return. Defaults to 10.",
-                    "default": 10
-                },
-                "assigned_to_fields": {
-                    "type": "string",
-                    "description": "This is a list of comma separated fields that the user wants to filter this request on."
-                }
-            },
-            "required": ["name"]
-        }
-    }
-})
-def get_br_assigned_to(name: str, limit: int = 10, assigned_to_fields: str = ""):
-    """
-    Gets BR information assigned to a given name.
-    """
-    fields = []
-    # If no specific fields are provided, use all valid fields
-    if assigned_to_fields:
-        assigned_to_fields_list = [field.strip() for field in assigned_to_fields.split(',')]
-        fields = extract_fields_from_query(assigned_to_fields_list, valid_assigned_to_fields)
-        # If no valid specific fields are provided, default to all valid fields
-        if not fields:
-            fields = valid_assigned_to_fields
-    else:
-        fields = valid_assigned_to_fields
+# @tool_metadata({
+#     "type": "function",
+#     "function": {
+#         "name": "get_br_assigned_to",
+#         "description": "Gets information about BRs assigned to a given name. If no limit is specified, it returns the top 10 items by default. List of fields a user can specify in assigned_to_fields: {fields}".replace("{fields}", ", ".join(valid_assigned_to_fields)),
+#         "parameters": {
+#             "type": "object",
+#             "properties": {
+#                 "name": {
+#                     "type": "string",
+#                     "description": "The name to search for in the BR records. This can be the BR owner, initiator, last editor, or other specified roles."
+#                 },
+#                 "limit": {
+#                     "type": "integer",
+#                     "description": "The maximum number of BR items to return. Defaults to 10.",
+#                     "default": 10
+#                 },
+#                 "assigned_to_fields": {
+#                     "type": "string",
+#                     "description": "This is a list of comma separated fields that the user wants to filter this request on."
+#                 }
+#             },
+#             "required": ["name"]
+#         }
+#     }
+# })
+# def get_br_assigned_to(name: str, limit: int = 10, assigned_to_fields: str = ""):
+#     """
+#     Gets BR information assigned to a given name.
+#     """
+#     fields = []
+#     # If no specific fields are provided, use all valid fields
+#     if assigned_to_fields:
+#         assigned_to_fields_list = [field.strip() for field in assigned_to_fields.split(',')]
+#         fields = extract_fields_from_query(assigned_to_fields_list, valid_assigned_to_fields)
+#         # If no valid specific fields are provided, default to all valid fields
+#         if not fields:
+#             fields = valid_assigned_to_fields
+#     else:
+#         fields = valid_assigned_to_fields
 
-    condition = " OR ".join([f"{field} LIKE %s" for field in fields])
-    query = f"""
-    WITH MatchedRows AS (
-        SELECT *
-        FROM EDR_CARZ.DIM_DEMAND_BR_ITEMS
-        WHERE {condition}
-    )
-    SELECT TOP(%d) *, (SELECT COUNT(*) FROM MatchedRows) AS TotalCount
-    FROM MatchedRows;
-    """
+#     condition = " OR ".join([f"{field} LIKE %s" for field in fields])
+#     query = f"""
+#     WITH MatchedRows AS (
+#         SELECT *
+#         FROM EDR_CARZ.DIM_DEMAND_BR_ITEMS
+#         WHERE {condition}
+#     )
+#     SELECT TOP(%d) *, (SELECT COUNT(*) FROM MatchedRows) AS TotalCount
+#     FROM MatchedRows;
+#     """
 
-    name_pattern = f"{name}%"
-    params = [name_pattern] * len(fields) + [limit]
-    result = db.execute_query(query, *params)
-    return {'br': result}
+#     name_pattern = f"{name}%"
+#     params = [name_pattern] * len(fields) + [limit]
+#     result = db.execute_query(query, *params)
+#     return {'br': result}
 
 @tool_metadata({
     "type": "function",
     "function": {
         "name": "search_br_by_field",
-        "description": "A 'BR' is a 'Business Request', which includes information about purchase orders inside the Government of Canada. This function searches information about BRs given a specific BR field (labelled `field_name`). If multiple fields are needed, invoke this function multiple times... once for each field. The fields available are: {fields}. If the user doesn't provide a `field_name` then let them know what the field names are. Otherwise use best guess.".replace("{fields}", ", ".join(valid_search_fields)),
+        "description": "A 'BR' is a 'Business Request', which includes information about purchase orders inside the Government of Canada. This function searches information about BRs given a specific BR field (labelled `field_name`). If multiple fields are needed, invoke this function multiple times... once for each field. The fields available are: {fields}. If the user doesn't provide a `field_name` then let them know what the field names are. Otherwise use best guess.".replace("{fields}", ", ".join(list(valid_search_fields.keys()))),
         "parameters": {
             "type": "object",
             "properties": {
@@ -130,29 +166,27 @@ def get_br_assigned_to(name: str, limit: int = 10, assigned_to_fields: str = "")
                 },
                 "limit": {
                     "type": "integer",
-                    "description": "The maximum number of BR items to return. Defaults to 10.",
-                    "default": 10
+                    "description": "The maximum number of BR items to return. Defaults to 50.",
+                    "default": 50
                 }
             },
             "required": ["field_name", "field_value"]
       }
     }
   })
-def search_br_by_field(field_name: str, field_value: str, limit: int = 10):
+def search_br_by_field(field_name: str, field_value: str, limit: int = 50):
     """
     search_br_by_field
 
     Search BRs via a specific field:
     """
     if field_name:
-        fields = extract_fields_from_query([field_name], valid_search_fields)
+        fields = extract_fields_from_query([field_name], list(valid_search_fields.keys()) )
         if fields:
-            query = """SELECT TOP(%d) *
-                       FROM EDR_CARZ.DIM_DEMAND_BR_ITEMS
-                       WHERE {field} LIKE %s;""".replace("{field}", fields[0])
+            query = _get_br_query(limit=bool(limit), by_field=valid_search_fields[fields[0]])
             result = db.execute_query(query, limit, f"%{field_value}%")
             return {'br': result}
-    return "Try using one of the following fields: " + ", ".join(valid_search_fields)
+    return "Try using one of the following fields: " + ", ".join(list(valid_search_fields.keys()))
 
 @tool_metadata({
     "type": "function",
@@ -212,7 +246,7 @@ def get_br_statuses(active: bool = True):
     """
 
     if active:
-        query += "\nWHERE BR_ACTIVE_EN = 'Active'"
+        query += "WHERE BR_ACTIVE_EN = 'Active'"
 
     result = db.execute_query(query)
     return {'br_statuses': result}
@@ -231,8 +265,8 @@ def get_br_statuses(active: bool = True):
                 },
                 "limit": {
                     "type": "integer",
-                    "description": "The maximum number of BR items to return. Defaults to 100.",
-                    "default": 100
+                    "description": "The maximum number of BR items to return. Defaults to 50.",
+                    "default": 50
                 },
                 "assigned_to": {
                     "type": "string",
@@ -243,7 +277,7 @@ def get_br_statuses(active: bool = True):
       }
     }
   })
-def get_br_by_status(status: str, assigned_to: str = "", limit: int = 100):
+def get_br_by_status(status: str, assigned_to: str = "", limit: int = 50):
     """
     This will retreive BR filtered by status.
     """
@@ -251,7 +285,11 @@ def get_br_by_status(status: str, assigned_to: str = "", limit: int = 100):
     result = db.execute_query(query, limit, status)
     return {'br': result}
 
-def _get_br_query(br_number_count: int = 0, status: bool = False, limit: bool = False, active: bool = False) -> str:
+def _get_br_query(br_number_count: int = 0,
+                    status: bool = False,
+                    limit: bool = False,
+                    active: bool = False,
+                    by_field: str = "") -> str:
     """Function that will build the select statement for retreiving BRs
 
     NOTE: No need to join on ORGANIZATION Table atm.. some info is already rolled in BR ITEMS ...
@@ -263,53 +301,10 @@ def _get_br_query(br_number_count: int = 0, status: bool = False, limit: bool = 
         query += "TOP(%d)\n"
 
     # Default select statement from BR_ITEMS & other tables
+    query += "br.BR_NMBR as BR_NMBR," + ",".join([f"{value} as {key}" for key, value in valid_search_fields.items()])
+
+    # Deault FROM statement
     query += """
-        br.BR_NMBR,
-        br.BR_TITLE,
-        br.BR_SHORT_TITLE,
-        br.RPT_GC_ORG_NAME_EN,
-        br.RPT_GC_ORG_NAME_FR,
-        br.ORG_TYPE_EN,
-        br.ORG_TYPE_FR,
-        br.REQST_IMPL_DATE,
-        br.BR_TYPE_EN,
-        br.BR_TYPE_FR,
-        br.PRIORITY_EN,
-        br.PRIORITY_FR,
-        br.BR_OWNER,
-        br.SUBMIT_DATE,
-        br.RVSD_TARGET_IMPL_DATE,
-        br.BA_OPI,
-        br.CPLX_EN,
-        br.CPLX_FR,
-        br.ACTUAL_IMPL_DATE,
-        br.AGRMT_END_DATE,
-        br.BA_TL,
-        br.TL_OPI,
-        br.SOL_OPI,
-        br.PM_OPI,
-        br.SCOPE_EN,
-        br.SCOPE_FR,
-        br.CSM_DIRTR,
-        br.CLIENT_REQST_SOL_DATE,
-        br.BA_PRICE_OPI,
-        br.SDM_TL_OPI,
-        br.CLIENT_SUBGRP_EN,
-        br.CLIENT_SUBGRP_FR,
-        br.PROD_OPI,
-        br.PRPO_TARGET_DATE,
-        br.IMPL_SGNOFF_DATE,
-        br.GROUP_EN,
-        br.GROUP_FR,
-        br.ENGN_OPI,
-        br.AGRMT_OPI,
-        br.BA_PRICE_OPI,
-        br.ASSOC_BRS,
-        br.REQMT_OVRVW,
-        s.BR_ACTIVE_EN,
-        s.BR_ACTIVE_FR,
-        s.BITS_STATUS_EN,
-        s.BITS_STATUS_FR
     FROM
         [EDR_CARZ].[DIM_DEMAND_BR_ITEMS] br
     """
@@ -344,7 +339,10 @@ def _get_br_query(br_number_count: int = 0, status: bool = False, limit: bool = 
         # Prevents SQL injection, this only calculates the placehoders ... i.e; BR_NMBR IN (%s, %s, %s)
         placeholders = ", ".join(["%s"] * br_number_count)
         base_where_clause.append(f"br.BR_NMBR IN ({placeholders})")
-    
+
+    if by_field:
+        base_where_clause.append(by_field + " LIKE %s")
+
     if base_where_clause:
         query += "WHERE " + " AND ".join(base_where_clause)
     return query
