6ea9bdab507c9371cdc9fa4927e579a08c50d3d0
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-04-10 08:39:41 -0400

    wip: removed unused variable tripping the build

--- app/frontend/src/stores/ChatStore.tsx
+++ app/frontend/src/stores/ChatStore.tsx
@@ -1,56 +1,69 @@
-import { create } from 'zustand';
-import { produce } from 'immer'
-import { DEFAULT_CHAT_MODEL } from '../constants/models';
-import { buildDefaultChatHistory, buildDefaultModel } from './modelBuilders';
+import { create } from "zustand";
+import { produce } from "immer";
+import { buildDefaultChatHistory, buildDefaultModel } from "./modelBuilders";
 
 type ChatStore = {
-    currentChatIndex: number,
-    currentChatHistory: ChatHistory,
-    chatHistoriesDescriptions: string[],
-    chatIndexToLoadOrDelete: number | null;
-    setChatIndexToLoadOrDelete: (index: number | null) => void;
-    setCurrentChatHistory: (param: ChatHistory | ((prev: ChatHistory) => ChatHistory)) => void;
-    setDefaultChatHistory: () => void;
-    getDefaultModel: () => string;
-    setCurrentChatIndex: (index: number) => void;
-    setChatHistoriesDescriptions: (descriptions: string[]) => void;
+  currentChatIndex: number;
+  currentChatHistory: ChatHistory;
+  chatHistoriesDescriptions: string[];
+  chatIndexToLoadOrDelete: number | null;
+  setChatIndexToLoadOrDelete: (index: number | null) => void;
+  setCurrentChatHistory: (
+    param: ChatHistory | ((prev: ChatHistory) => ChatHistory)
+  ) => void;
+  setDefaultChatHistory: () => void;
+  getDefaultModel: () => string;
+  setCurrentChatIndex: (index: number) => void;
+  setChatHistoriesDescriptions: (descriptions: string[]) => void;
 };
 
 export const useChatStore = create<ChatStore>((set) => ({
-    currentChatIndex: 0,
-    chatIndexToLoadOrDelete: null,
-    currentChatHistory: buildDefaultChatHistory(),
-    chatHistoriesDescriptions: ["Conversation 1"],
-    setChatIndexToLoadOrDelete: (index: number | null) => {
-        set((state) => produce(state, (draft) => {
-            draft.chatIndexToLoadOrDelete = index;
-        }));
-    },
-    setCurrentChatHistory: (param: ChatHistory | ((prev: ChatHistory) => ChatHistory)) => {
-        set((state) => produce(state, (draft) => {
-            if (typeof param === "function") {
-                draft.currentChatHistory = param(draft.currentChatHistory);
-            } else {
-                draft.currentChatHistory = param;
-            }
-        }));
-    },
-    setDefaultChatHistory: () => {
-        set((state) => produce(state, (draft) => {
-            draft.currentChatHistory = buildDefaultChatHistory();
-        }));
-    },
-    getDefaultModel: () => {
-        return buildDefaultModel();
-    },
-    setCurrentChatIndex: (index: number) => {
-        set((state) => produce(state, (draft) => {
-            draft.currentChatIndex = index;
-        }));
-    },
-    setChatHistoriesDescriptions: (descriptions: string[]) => {
-        set((state) => produce(state, (draft) => {
-            draft.chatHistoriesDescriptions = descriptions;
-        }));
-    }
+  currentChatIndex: 0,
+  chatIndexToLoadOrDelete: null,
+  currentChatHistory: buildDefaultChatHistory(),
+  chatHistoriesDescriptions: ["Conversation 1"],
+  setChatIndexToLoadOrDelete: (index: number | null) => {
+    set((state) =>
+      produce(state, (draft) => {
+        draft.chatIndexToLoadOrDelete = index;
+      })
+    );
+  },
+  setCurrentChatHistory: (
+    param: ChatHistory | ((prev: ChatHistory) => ChatHistory)
+  ) => {
+    set((state) =>
+      produce(state, (draft) => {
+        if (typeof param === "function") {
+          draft.currentChatHistory = param(draft.currentChatHistory);
+        } else {
+          draft.currentChatHistory = param;
+        }
+      })
+    );
+  },
+  setDefaultChatHistory: () => {
+    set((state) =>
+      produce(state, (draft) => {
+        draft.currentChatHistory = buildDefaultChatHistory();
+      })
+    );
+  },
+  getDefaultModel: () => {
+    return buildDefaultModel();
+  },
+  setCurrentChatIndex: (index: number) => {
+    set((state) =>
+      produce(state, (draft) => {
+        draft.currentChatIndex = index;
+      })
+    );
+  },
+  setChatHistoriesDescriptions: (descriptions: string[]) => {
+    set((state) =>
+      produce(state, (draft) => {
+        draft.chatHistoriesDescriptions = descriptions;
+      })
+    );
+  },
 }));
