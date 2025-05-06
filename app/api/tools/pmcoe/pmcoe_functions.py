092295ab51a88309333f27bbe907c941355bfc85
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-05-06 11:15:33 -0400

    wip: added pmcoe as a standalone tool, fixed github action bootstrap for enabled tools

---
+++ app/api/tools/pmcoe/pmcoe_functions.py
@@ -0,0 +1,28 @@
+import logging
+
+from utils.decorators import tool_metadata
+
+__all__ = ["pmcoe_question"]
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+@tool_metadata({
+    "type": "function",
+    "function": {
+        "name": "pmcoe_question",
+        "description": "The library contains bilingual gate templates (French and English), as well as Shared Services Canada (SSC) project management artifacts and standardized templates to support consistent project delivery and documentation.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "query": {
+                    "type": "string",
+                    "description": "The question that relates to anything related to project managment or gate templates within SSC"
+                }
+            }
+        }
+    }
+})
+def pmcoe_question(query: str): # pylint: disable=unused-argument
+    """returns the name of the telecom index name"""
+    return "pmcoe"
