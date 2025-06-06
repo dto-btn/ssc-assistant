c3acc6018fd5a3d272d72bf0039ced957dfbb1b5
Monarch Wadia <monarchwadia@gmail.com>
2025-06-06 10:26:37 -0400

    Updates to disclaimer code

--- app/frontend/src/components/Disclaimer.tsx
+++ app/frontend/src/components/Disclaimer.tsx
@@ -8,97 +8,96 @@ import {
 } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
-import Cookies from "js-cookie";
 import { useMsal } from "@azure/msal-react";
 import { InteractionStatus } from "@azure/msal-browser";
 import { useAppStore } from "../stores/AppStore";
 import { allowedToolsSet } from "../allowedTools";
+import { DisclaimerKey, DisclaimerState } from "../../types";
+import { PersistenceUtils } from '../util/persistence';
 
-interface Disclaimer {
-  key: string;
+interface DisclaimerConfig {
+  key: DisclaimerKey;
   text: string;
   text2: string;
   title?: string;
   cookieName: string;
-  accepted: boolean;
+  // Additional properties for when to require acceptance of the disclaimer
+  requiredWhen?: () => boolean;
 }
+
+const DISCLAIMER_CONFIG: DisclaimerConfig[] = [
+  {
+    key: DisclaimerKey.main,
+    text: "disclaimer.desc",
+    text2: "disclaimer.desc2",
+    cookieName: "disclaimer_accepted",
+  },
+  {
+    key: DisclaimerKey.br,
+    text: "br.disclaimer",
+    text2: "br.disclaimer2",
+    title: "bits",
+    cookieName: "br_disclaimer_accepted",
+    requiredWhen: () => allowedToolsSet.has("bits"),
+  }
+]
 
 export const Disclaimer = () => {
   const { t } = useTranslation();
-  const [hideDialog, setHideDialog] = useState(true);
   const { inProgress } = useMsal();
   const appStore = useAppStore();
 
-  const brEnabled: boolean = allowedToolsSet.has("bits");
-  const [disclaimers, setDisclaimers] = useState<Disclaimer[]>([
-    {
-      key: "main",
-      text: "disclaimer.desc",
-      text2: "disclaimer.desc2",
-      cookieName: "disclaimer_accepted",
-      accepted: Cookies.get("disclaimer_accepted") === "true",
-    },
-    {
-      key: "br",
-      text: "br.disclaimer",
-      text2: "br.disclaimer2",
-      title: "bits",
-      cookieName: "br_disclaimer_accepted",
-      accepted: brEnabled
-        ? Cookies.get("br_disclaimer_accepted") === "true"
-        : true,
-    },
-  ]);
+  // Keep track of the disclaimers and their acceptance state
+  const [disclaimerAcceptedState, setDisclaimerAcceptedState] = useState<DisclaimerState>(
+    () => PersistenceUtils.getDisclaimerAcceptedState()
+  );
 
-  // Find the first unaccepted disclaimer
-  const currentDisclaimer = disclaimers.find((d) => !d.accepted);
-
+  // Find the first disclaimer that is not accepted, based on disclaimerAcceptedState
+  const [currentDisclaimerKey, setCurrentDisclaimerKey] = useState<DisclaimerKey | null>(null);
   useEffect(() => {
-    if (
-      !disclaimers.every((d) => d.accepted) &&
-      inProgress === InteractionStatus.None
-    ) {
-      setHideDialog(false);
-    }
-  }, [disclaimers, inProgress]);
-
-  const handleAccept = (key: string) => {
-    const updatedDisclaimers = disclaimers.map((d) => {
-      if (d.key === key) {
-        setDisclaimerCookie(d.cookieName); // Call your function here
-        return { ...d, accepted: true };
-      }
-      return d;
+    const currentDisclaimer = DISCLAIMER_CONFIG.find((d) => {
+      const isAccepted = disclaimerAcceptedState[d.key];
+      const meetsConditions = !d.requiredWhen || d.requiredWhen();
+      return !isAccepted && meetsConditions;
     });
-    setDisclaimers(updatedDisclaimers);
+    setCurrentDisclaimerKey(currentDisclaimer?.key || null);
+  }, [disclaimerAcceptedState]);
+
+  const shouldShow = !!currentDisclaimerKey && inProgress === InteractionStatus.None;
+
+  const handleAccept = (key: DisclaimerKey) => {
+    PersistenceUtils.setDisclaimerAccepted(key);
+    setDisclaimerAcceptedState((prevState) => ({
+      ...prevState,
+      [key]: true,
+    }));
   };
 
-  const setDisclaimerCookie = (cookie_name: string) => {
-    Cookies.set(cookie_name, "true", {
-      expires: 30, // 30 days
-    });
-  };
+  const currentDisclaimerConfig = DISCLAIMER_CONFIG.find((d) => d.key === currentDisclaimerKey);
+
+  if (!currentDisclaimerConfig) {
+    return null; // No configuration found for the current disclaimer
+  }
 
   return (
     <div>
-      <Dialog open={!hideDialog && !!currentDisclaimer} fullWidth>
+      <Dialog open={shouldShow} fullWidth>
         <DialogTitle>
-          {currentDisclaimer?.title
-            ? t("disclaimer") + " - " + t(currentDisclaimer.title)
+          {currentDisclaimerConfig?.title
+            ? t("disclaimer") + " - " + t(currentDisclaimerConfig.title)
             : t("disclaimer")}
         </DialogTitle>
         <DialogContent>
-          {currentDisclaimer?.text && <p>{t(currentDisclaimer.text)}</p>}
-          {currentDisclaimer?.text2 && (
-            <p style={{ fontWeight: "bold" }}>{t(currentDisclaimer.text2)}</p>
+          {currentDisclaimerConfig?.text && <p>{t(currentDisclaimerConfig.text)}</p>}
+          {currentDisclaimerConfig?.text2 && (
+            <p style={{ fontWeight: "bold" }}>{t(currentDisclaimerConfig.text2)}</p>
           )}
         </DialogContent>
         <DialogActions>
           <Button
-            style={{ backgroundColor: "#4b3e99", color: "white" }}
-            onClick={() =>
-              currentDisclaimer && handleAccept(currentDisclaimer.key)
-            }
+            color='primary'
+            variant='contained'
+            onClick={() => handleAccept(currentDisclaimerConfig.key)}
           >
             {t("accept")}
           </Button>
