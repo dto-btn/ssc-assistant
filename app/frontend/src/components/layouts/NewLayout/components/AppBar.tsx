d2273821d447e54b8d662777d9ef0a9cb785ab52
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-06-11 08:20:43 -0400

    Fixed 3 console error from props being passed and react not liking them and one for missing key on the tool/profile menu

--- app/frontend/src/components/layouts/NewLayout/components/AppBar.tsx
+++ app/frontend/src/components/layouts/NewLayout/components/AppBar.tsx
@@ -2,32 +2,27 @@ import styled from "@mui/material/styles/styled";
 import MuiAppBar, { AppBarProps } from "@mui/material/AppBar";
 
 interface NewAppBarProps extends AppBarProps {
-  isOpen: boolean,
-  drawerWidth: number,
+  isOpen: boolean;
+  drawerWidth: number;
 }
 
 export const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<NewAppBarProps>(({ theme, drawerWidth }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
+  shouldForwardProp: (prop) => prop !== "isOpen" && prop !== "drawerWidth",
+})<NewAppBarProps>(({ theme, isOpen, drawerWidth }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  variants: [
-    {
-      props: ({ isOpen }) => isOpen,
-      style: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: `${drawerWidth}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-        '& .MuiToolbar-gutters': {
-          paddingLeft: theme.spacing(1),
-          paddingRight: theme.spacing(1),
-        }
-      },
+  ...(isOpen && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    "& .MuiToolbar-gutters": {
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
     },
-  ],
+  }),
 }));
