53970431b7e6f04df1bc11a3f3d0b758262554ea
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-06-27 09:47:24 -0400

    Added missing tf config for prod (#593)

--- terraform/azure_functions.tf
+++ terraform/azure_functions.tf
@@ -1,11 +1,20 @@
 /****************************************************
 *                  Function App                     *
 *****************************************************/
+resource "azurerm_log_analytics_workspace" "functions" {
+  name                = "functions-app-workspace"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_application_insights" "functions" {
   name                = "functions-app-insights"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.functions.id
 }
 
 resource "azurerm_service_plan" "functions" {
