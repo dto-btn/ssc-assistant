7c64fe9a5d64d5b4dfc1e6d31963420e6f1928b2
jeneerthan7 <jeneerthan.pageerathan@ssc-spc.gc.ca>
2025-07-30 14:10:32 -0400

    Commented out state change to allow for delay after stop button

--- app/frontend/src/screens/MainScreen/useApiRequestService.ts
+++ app/frontend/src/screens/MainScreen/useApiRequestService.ts
@@ -176,7 +176,7 @@ export const useApiRequestService = () => {
 
     const abortRequest = () => {
         abortRef.current = true;
-        setIsLoading(false); // Is this needed? 
+        // setIsLoading(false); // Is this needed? 
         // It avoids delay in transition after stop button click, 
         // but can cause issues if users asks a follow up question quickly after
     };
