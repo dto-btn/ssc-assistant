eeb5431304095be631ecf642fabe34c185e33086
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-09-10 11:07:01 -0400

    Added playground route and agent link for pmcoe

---
+++ app/frontend/src/routes/PlaygroundRoute.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Playground from '../screens/Playground';
+
+interface PlaygroundProps {
+    /* props */
+}
+
+const PlaygroundRoute: React.FC<PlaygroundProps> = ({ /* props */ }) => {
+    return (
+        <Playground />
+    );
+};
+
+export default PlaygroundRoute;
