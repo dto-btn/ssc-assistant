5e6c2986e860d0eb5f8b7a157555218417a34ee3
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-10-17 09:32:42 -0400

    676 modify UI to distinguish production environment from dev (#681)
    * added dev banner to the site
    
    * rewording to just DEV

--- terraform/dev/frontend.tf
+++ terraform/dev/frontend.tf
@@ -51,6 +51,7 @@ resource "azurerm_linux_web_app" "frontend" {
     MICROSOFT_PROVIDER_AUTHENTICATION_SECRET = var.microsoft_provider_authentication_secret
     PORT = 8080
     WEBSITE_AUTH_AAD_ALLOWED_TENANTS = data.azurerm_client_config.current.tenant_id
+    VITE_SHOW_DEV_BANNER=true
   }
 
   sticky_settings {
