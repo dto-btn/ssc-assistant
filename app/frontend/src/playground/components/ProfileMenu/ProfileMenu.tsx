845322d355c721d07673d2ca47e9871d92e06a85
bigyam <ruana@SSC-3CJR814.localdomain>
2025-11-04 11:19:06 -0500

    some skellies: userslice and main profile component

---
+++ app/frontend/src/playground/components/ProfileMenu/ProfileMenu.tsx
@@ -0,0 +1,26 @@
+/**
+ * ProfileMenu component
+ * 
+ * Blurb here
+ */
+
+/**
+ * Prop for {@link }
+ * 
+ * Add prop desc
+ */
+interface ProfileMenuProps {
+
+}
+
+/**
+ * Dev blurb on component?  what's the diff with this and the one up top?
+ */
+const ProfileMenu: React.FC<ProfileMenuProps> = ({}) => {
+
+    return (
+        <></>
+    )
+}
+
+export default ProfileMenu;
