fa06f729c1d653855637a5c78e3391faf8b4eed5
Guillaume Turcotte <leguille.ftw@gmail.com>
2025-12-02 14:13:52 -0500

    removing tokenPRovider as it's not used

--- app/frontend/src/playground/components/PlaygroundRoot.tsx
+++ app/frontend/src/playground/components/PlaygroundRoot.tsx
@@ -14,7 +14,6 @@ import { useAuth } from "../store/hooks/useAuth";
 import { apiUse } from "../../authConfig";
 import isFeatureEnabled from "../FeatureGate";
 import { DevBanner } from "./DevBanner";
-import TokenProvider from "./TokenProvider";
 import SessionBootstrapper from "./SessionBootstrapper";
 
 const PlaygroundRoot: React.FC = () => {
@@ -23,7 +22,6 @@ const PlaygroundRoot: React.FC = () => {
 
   return (
     <>
-      <TokenProvider />
       <SessionBootstrapper />
       <Box display="flex" height="100vh">
         <SessionSidebar />
