661c0d7a7436b9f8d6250e4e56e2a00a4781e9ed
jeneerthan <jeneerthan.pageerathan@ssc-spc.gc.ca>
2026-05-01 14:45:13 -0400

    ignore playwright tests for vitest runs

--- app/frontend/vitest.config.ts
+++ app/frontend/vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
         setupFiles: ['./vitest.setup.ts'],
         globals: true,
         environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'tests/e2e/**'],
     },
 })
 
